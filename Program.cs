fc4cb6d88e0cba4794fb17eb784e13d6071cf18e
Edenathan256 <lancemeganratilla@gmail.com>
2025-08-17 17:17:33 +0900

    Initial Release
    Please read the README.md for usage information.

---
+++ Program.cs
@@ -0,0 +1,284 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Threading;
+using NAudio.Midi;
+
+// This program is licensed under the Creative Commons
+// Attribution-NonCommercial-ShareAlike 4.0 International License.
+// To view a copy of this license, visit
+// http://creativecommons.org/licenses/by-nc-sa/4.0/
+
+// Created by Daniferous, with the use of NAudio by Mark Heath.
+// See Credits in the README.md.
+
+namespace FaeHZMIDI
+{
+    class Program
+    {
+        [STAThread]
+        static void Main(string[] args)
+        {
+            try
+            {
+                switch (args.Length)
+                {
+                    case 0:
+                        Console.WriteLine($"Please drag and drop (or run this program with) a MIDI file here.\nHere's a scarameow:\n/ᐠ - ˕ -マ");
+                        Console.ReadLine();
+                        break;
+                    case 1:
+                        Console.WriteLine("Here's a scarameow:\n/ᐠ - ˕ -マ");
+                        AnalyzeFile(args[0]);
+                        break;
+                    default:
+                        Console.WriteLine($"Multiple file analysis not supported.\nHere's a scarameow:\n/ᐠ - ˕ -マ");
+                        Console.ReadLine();
+                        break;
+                }
+            }
+            catch (Exception dead)
+            {
+                Console.WriteLine($"Something went terribly wrong:\n{dead.Message}\n");
+                Console.ReadLine();
+            }
+        }
+        
+        private static void AnalyzeFile(string file)
+        {
+            if (File.Exists(file))
+            {
+                string? dir = Path.GetDirectoryName(file);
+                if (string.IsNullOrEmpty(dir))
+                {
+                    dir = Environment.CurrentDirectory;
+                }
+                string outfile = Path.Combine(dir,Path.GetFileNameWithoutExtension(file) + " - converted.mid");
+
+                // Here we go...
+                try
+                {
+                    var midi = new MidiFile(file,false);
+
+                    var tempoList = new List<(long tick, int microsecondsPerQuart)>();
+                    foreach (var a in midi.Events[0]) // Remember that all Tempos are on Track 0.
+                    {
+                        if (a is TempoEvent tempo)
+                        {
+                            tempoList.Add((tempo.AbsoluteTime, tempo.MicrosecondsPerQuarterNote));
+                        }
+                    }
+                    tempoList = tempoList.OrderBy(tem => tem.tick).ToList(); // Probably not needed, as the tempo events are already sorted by tick, and there is only one track.
+
+                    // List of notes pre-conversion
+                    var noteListOriginal = new List<(NoteOnEvent note_on, NoteEvent note_off, int track, bool doFreq, double coefficientWrap, int deltaOctave)>();
+
+
+                    /*******************************************************
+                        The following are default values for parameters.
+                    *******************************************************/
+
+                    // Determining which notes to divide by a nonzero value and multiply finite times.
+                    bool DoFreq = false;
+                    // Wrap Frequencies (Prevents ultra-long and ultra-short notes.)
+                    bool WrapFreq = false;
+                    // Set Octave Delta (Multiplies/Divides note lengths)
+                    int DeltaOctave = -3;
+                    // Set Wrap Coefficient (Proportion of Time Interval to Wrap Frequencies with)
+                    double CoefficientWrap = 0.01;
+
+                    // Generic isNumerical Boolean
+                    bool genericIsNumerical = true;
+                    double genericNumber = 0;
+
+                    // Iterate through events of each track.
+                    for (int trk = 0; trk < midi.Tracks; trk++)
+                    {
+                        foreach (var ev in midi.Events[trk])
+                        {
+                            if (ev is TextEvent text) // I do not know any better method than this...
+                            {
+                                switch (text.Text)
+                                {
+                                    case "beginFreq":
+                                        DoFreq = true;
+                                        Console.WriteLine($"Will now convert notes starting at Track {trk} at Position {text.AbsoluteTime}.");
+                                        break;
+                                    case "endFreq":
+                                        DoFreq = false;
+                                        Console.WriteLine($"Will no longer convert notes starting at Track {trk} at Position {text.AbsoluteTime}.");
+                                        break;
+                                    case "enableFreqWrap":
+                                        WrapFreq = true;
+                                        Console.WriteLine($"Enabled Frequency Wrapping.");
+                                        break;
+                                    default:
+                                        if ((text.Text).Length > 16)
+                                        {
+                                            if ((text.Text).Substring(0,15) == "coefficientWrap")
+                                            {
+                                                genericIsNumerical = double.TryParse((text.Text).Substring(16), out genericNumber);
+                                                if (genericIsNumerical)
+                                                {
+                                                    if (genericNumber > 0 && genericNumber < 1)
+                                                    {
+                                                        CoefficientWrap = genericNumber;
+                                                        Console.WriteLine($"Set the Wrapping Coefficient to {CoefficientWrap}.");
+                                                    }
+                                                }
+                                            }
+                                        }
+                                        if ((text.Text).Length > 10)
+                                        {
+                                            if ((text.Text).Substring(0,9) == "octaveSet")
+                                            {
+                                                genericIsNumerical = double.TryParse((text.Text).Substring(10), out genericNumber);
+                                                if (genericIsNumerical)
+                                                {
+                                                    if (genericNumber > -8 && genericNumber < 7)
+                                                    {
+                                                        DeltaOctave = (int)Math.Round(genericNumber);
+                                                        Console.WriteLine($"Set the Octave Offset to {DeltaOctave}.");
+                                                    }
+                                                }
+                                            }
+                                        }
+                                        break;
+                                }
+                            }
+                            if (ev is NoteOnEvent note && note.Velocity > 0 && note.OffEvent is NoteEvent note_off) // FL Studio Users: PLEASE DO NOT enable 0 velocity notes.
+                            {
+                                noteListOriginal.Add((note,note_off,trk,DoFreq,CoefficientWrap,DeltaOctave)); // To do: Add DeltaOctave and CoefficientWrap to NoteListOriginal
+                            }
+                        }
+                    }
+
+                    // Strict Note Limit (Max limit to prevent large notecounts as a result!)
+                    // Only increases as the number of repeated notes increases.
+                    // Is not influenced by the MIDI's actual note count.
+                    long note_limit = 16777215; long nc = 0;
+
+                    // Dealing with notes, finally!
+                    foreach (var (note_on, note_off, track, doFreq, coefficientWrap, deltaOctave) in noteListOriginal)
+                    {
+                        // Tempo at note start...
+                        // This is ineffective if the tempo changes rapidly,
+                        // So MIDIs containing BPM Jumps, Ramps, Spirals, Dodecahedrons are discouraged with this program.
+                        int microsecondsPerQuart = TempoAtTick(note_on.AbsoluteTime, tempoList);
+                        double BPM = 60000000.0 / microsecondsPerQuart;
+                        double BPS = 1000000.0 / Math.Floor((60000000.0 / BPM) + 0.5);
+
+                        // Tick Interval (PPQ of MIDI x Beats per Seconds)
+                        double tInt = (midi.DeltaTicksPerQuarterNote * BPS);
+
+                        // Frequency of a note (assuming a Tick Interval of exactly 1):
+                        // Frequency(Note) = 440 * 2^((Note - 69)/12) * 2^(Octave Change)
+                        // Octave Change is by default -3.
+                        // Lower values = Lower Frequencies and Longer Divisions
+                        // Higher values = Not recommended for small PPQs.
+                        // Should I increase the resolution of the MIDI on demand?
+                        // Anyways, step (note length):
+                        double step = tInt / NoteFrequency(note_on.NoteNumber, deltaOctave);
+
+                        if (WrapFreq)
+                        {
+                            if (!(coefficientWrap > 0 && coefficientWrap < 1))
+                            {
+                                double step_old = step;
+                                step = step_old / (Math.Pow(2, Math.Ceiling(Math.Log(step_old / (0.01 * tInt), 2))));
+                                Console.WriteLine($"Warning: Invalid Wrapping Coefficient.\nCoefficient must be greater than 0 but lesser than 1.\nWill use a Wrapping of Coefficient value of 0.01.");
+                            }
+                            else
+                            {
+                                double step_old = step;
+                                step = step_old / (Math.Pow(2, Math.Ceiling(Math.Log(step_old / (coefficientWrap * tInt), 2))));
+                            }                            
+                        }
+
+                        if (step > 1 && doFreq && (nc <= note_limit))
+                        {
+                            int rep = (int)Math.Ceiling((note_off.AbsoluteTime - note_on.AbsoluteTime)/step);
+                            double time_start = note_on.AbsoluteTime;
+
+                            // Making the notes to make the notes...
+                            for (int note_add = 0; note_add < rep; note_add++)
+                            {
+                                var new_on = new NoteOnEvent((long)Math.Round(time_start), note_on.Channel, note_on.NoteNumber, note_on.Velocity, (int)Math.Ceiling(step));
+                                var new_off = new NoteEvent((long)Math.Round(time_start + step), note_on.Channel, MidiCommandCode.NoteOff, note_on.NoteNumber, 0);
+                                midi.Events[track].Add(new_on);
+                                midi.Events[track].Add(new_off);
+                                time_start += step;
+                                nc++;
+                            }
+
+                            // Remove after finishing
+                            midi.Events[track].Remove(note_on);
+                            midi.Events[track].Remove(note_off);
+                        }
+                    }
+
+                    // Sorting the list
+                    for (int trk = 0; trk < midi.Tracks; trk++)
+                    {
+                        var scarameowList = midi.Events[trk].OrderBy(scarameow => scarameow.AbsoluteTime).ToList();
+                        midi.Events[trk].Clear();
+                        foreach (var kazuha in scarameowList)
+                        {
+                            midi.Events[trk].Add(kazuha);
+                        } 
+                    }
+
+                    // Saving, probably
+                    if (nc > note_limit)
+                    {
+                        Console.WriteLine($"Warning: Some notes were skipped due to have exceeded the notecount limit of {note_limit}.\nThe notecount is independent of the MIDI's actual note count and only counts the repeated notes.");
+                        Console.ReadLine();
+                    }
+                    else if (nc == 0)
+                    {
+                        Console.WriteLine($"Warning: No notes were converted. Did you set a control boundary?\nUse \"beginFreq\" and \"endFreq\" to set a control boundary and try again.");
+                        Console.ReadLine();
+                    }
+                    MidiFile.Export(outfile, midi.Events);
+                    Console.WriteLine($"MIDI exported successfully!\n{outfile}\n");
+                    Console.ReadLine();
+                }
+                catch (Exception dead)
+                {
+                    Console.WriteLine($"Something went terribly wrong:\n{dead.Message}\n");
+                    Console.ReadLine();
+                }
+            }
+            else
+            {
+                Console.WriteLine($"No MIDI detected...");
+                Console.ReadLine();
+            }
+        }
+
+        static int TempoAtTick(long note_tick, List<(long tick, int microsecondsPerQuart)> tempoList)
+        {
+            int microsecondsPerQuart = 500000; // 120 BPM
+            foreach (var (tick, tempo) in tempoList)
+            {
+                if (note_tick >= tick)
+                {
+                    microsecondsPerQuart = tempo;
+                }
+                else
+                {
+                    break;
+                }
+            }
+            return microsecondsPerQuart;
+        }
+        
+        static double NoteFrequency(int note, int octave) // By default, octave is 0.
+        {
+            return 440.0 * Math.Pow(2.0, ((note - 69) / 12.0)) * Math.Pow(2.0, octave);
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,49 @@
 # FaeleiHZMIDI
-Using keywords "beginFreq" and "endFreq", you can now quickly make MIDI notes have the rendered HZ feel.
+## About
+
+<div style="text-align: justify;">
+
+*A quick and versatile tool to make those trendy HZ Bass in your MIDIs, be it an "impossible remix" or not!*
+
+This page uses the term "Hertzchop" which means "to chop a MIDI note or set of MIDI notes into a sequence of notes, that when played, resonates a frequency that aligns to its pitch (`NoteNumber`).
+
+## Usage
+Faelei HZ MIDI uses `textEvent` MIDI Events to determine which sections of the MIDI to be Hertzchopped. 
+
+If you want an even quicker tool (that Hertzchops the ENTIRE MIDI and not just sections), use this [hzchop tool made by GamingMIDI from the BMC](https://cdn.discordapp.com/attachments/342003805270966284/1367762328690626640/hzchop.exe?ex=68a22b2e&is=68a0d9ae&hm=be7fdfe9bf5f91fad0c67a5823affac0ec42b86991ee49405d59187017b51121&) instead. 
+
+See below for the Keywords used:
+
+
+
+| Keywords | Actions                                                                                                                                                                                                                     | Usage                                                                                                                                                                       | Tips                                                                                                                                                                                                                                               |
+|-------------------------|-----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------|-----------------------------------------------------------------------------------------------------------------------------------------------------------------------------|----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------|
+| beginFreq               | Marks the start of the Hertzchop Section. Notes past this event (and track the event is on) will be Hertzchopped.                                                                                                           | Place this event before the set of notes you want to Hertzchop.                                                                                                             | Sections can span thru tracks, just make sure that the beginFreq event is located at the first track of the Hertzchop Section and the endFreq event is located at the last track.<br/><br/>There can be multiple Hertzchop sections, not just one! |
+| endFreq                 | Marks the end of the Hertzchop Section. Notes past this event (and track the event is on) will no longer be Hertzchopped.                                                                                                   | Place this event after the set of notes you want to Hertzchop.                                                                                                              |                                                                                                                                                                                                                                                    |
+| octaveSet [octave]      | Sets the deltaOctave variable to [octave]. Higher values will yield higher frequencies and lower note lengths. It is recommended to have this value set to any value under zero, especially for MIDIs with low resolutions. | Place this event before the set of notes you want to Hertzchop. Alternatively, this event can be placed on the first track if the settings will be used for the whole MIDI. | There can be multiple octaveSet events anywhere, meaning you can configure a track dedicated for bass to have a higher frequency, and the track dedicated for melody to have a lower frequency.                                                    |
+| enableFreqWrap          | Enables Frequency Wrapping by a Wrapping Coefficient to ensure that the output length of Hertzchopped notes are within allowable ranges.                                                                                    | Place this event at the first track. This feature is a global feature and cannot be disabled once enabled.                                                                  | You can use this, in conjunction with octaveSet and coefficientWrap events for even more customization.                                                                                                                                            |
+| coefficientWrap [value] | Sets the Wrapping Coefficient to [value]. This value has to be between 0 and 1.                                                                                                                                             | Place this event at the first track. This can be placed before a Hertzchop section.                                                                                         | There can be multiple coefficientWrap events everywhere, for similar reasons stated in the octaveSet tips section.                                                                                                                                 |
+
+
+In summary, Using keywords `beginFreq` and `endFreq`, you can now quickly make MIDI notes have the rendered HZ feel.
+
+## FAQ
+
+<ol>
+<li><b>When I run the program, it shows an error. Why does this happen?</b><br><em>You need to drag and drop the MIDI to the program itself. Then the program will work.</em></li>
+<li><b>When I run the program with the MIDI, nothing changes! No MIDIs were "Hertzchopped" and I got a warning telling me so. Is there a fix?</b><br><em>You did not add the keywords in the form of text events in your MIDI. Make sure your MIDI contains the keywords before you run the program with it.</em></li>
+<li><b>When I run the program with the MIDI, I receive an error that talks about a </b><span style="font-family: 'Lucida Console', Monaco, monospace;background-color: #707070;padding: 2px;">Note Off without a Note On</span><b>. What can I do?</b><br><em>This is a limitation of NAudio and not my program. While I am working on making my own MIDI File Reader to hopefully ignore this issue, you would have to load the MIDI in a MIDI Editor and re-export it back. This is the best method I can recommend in the meantime.</em></li>
+<li><b>Where can I put a text event on my MIDI?</b><br><em>You have to use either MIDIEditor or Domino MIDI Editor programs. For Domino, head to </em><span style="font-family: 'Lucida Console', Monaco, monospace;background-color: #707070;padding: 2px;">Insert > Lyrics</span><em> and then add the keyword. It does not matter if the text event is a Lyrics event, it will work. Theoretically, you could set the track name to be one of the keywords and it could potentially work.</em></li>
+<li><b>The MIDI is finally Hertzchopped. Is it openable in MIDI Editors?</b><br><em>Now that your MIDI has been Hertzchopped, it should be openable in MIDI Editors as only note events were manipulated.</em></li>
+<li><b>Will you add the option to change the MIDI Resolution?</b><br><em>I might add an option to set the resolution of the output MIDI, which is available in GamingMIDI's hzchop program. However, considering that this tool is intended to be used while editing or making MIDIs, you could simply change the resolution of the MIDI in the MIDI Editor instead.</em></li>
+</ol>
+
+## Credits
+
+"NAudio" is created and copyrighted by Mark Heath and contributors.
+
+"FaeleiHZMIDI" is created by Daniferous and is under a [Creative Commons License BY-NC-SA](https://creativecommons.org/licenses/by-nc-sa/4.0/).
+
+<img src="https://mirrors.creativecommons.org/presskit/buttons/88x31/png/by-nc-sa.png" alt="Creative Commons License BY-NC-SA" width="161" height="56">
+
+</div>
